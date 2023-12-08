796de4fc989f8999a5e8adde95686714add37d29
caiofu <caiofu@hotmail.com>
2023-12-08 00:41:13 -0300

    final

--- app/exemplo2/page.tsx
+++ app/exemplo2/page.tsx
@@ -4,34 +4,30 @@ type Time = {
   datetime: string
 }
 
-async function getTime(): Promise<Time> {
+async function getTime(): Promise<Time> 
+{
   const res = await fetch('http://www.worldtimeapi.org/api/timezone/America/Sao_Paulo')
-
   return  res.json()
-  
 }
-export default async function Exemplo1() {
+
+export default async function Exemplo1() 
+{
   const [time] = await Promise.all([getTime()])
-  // Converte a string de data em um objeto Date
-  const dateTime = new Date(time.datetime);
- //Formata a data e hora usando as opções de localização do Brasil
-  let dataHoraFormatada = dateTime.toLocaleString('pt-BR');
-  dataHoraFormatada = dataHoraFormatada.replace(/,/g, ' |');
-  const refreshPage = () => {
-    window.location.reload();
-  };
+    // Converte a string de data em um objeto Date
+    const dateTime = new Date(time.datetime);
+    //Formata a data e hora usando as opções de localização do Brasil
+    let dataHoraFormatada = dateTime.toLocaleString('pt-BR');
+    dataHoraFormatada = dataHoraFormatada.replace(/,/g, ' |');
 
   return (
    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', flexDirection:'column' }}>
-  
+    <p className="titulo">Exemplo sem validate</p>
     <div>
       <p className="horaP">{dataHoraFormatada}</p>
-
-      
     </div>
+
     <div className="divLink" style={{ }} >
         <Link className="link" href="/">Voltar</Link>
-   
       </div>
    </div>
   )
