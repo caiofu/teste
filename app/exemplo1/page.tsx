0ef091b806bfb0330259d77f6b8a368ce85fcaad
caiofu <caiofu@hotmail.com>
2023-12-07 23:48:14 -0300

    Exemplos

---
+++ app/exemplo1/page.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link"
+
+type Time = {
+  datetime: string
+}
+
+async function getTime(): Promise<Time> {
+  const res = await fetch('http://www.worldtimeapi.org/api/timezone/America/Sao_Paulo', {next: {revalidate: 25}})
+
+  return  res.json()
+  
+}
+export default async function Exemplo1() {
+  const [time] = await Promise.all([getTime()])
+  // Converte a string de data em um objeto Date
+  const dateTime = new Date(time.datetime);
+ //Formata a data e hora usando as opções de localização do Brasil
+  let dataHoraFormatada = dateTime.toLocaleString('pt-BR');
+  dataHoraFormatada = dataHoraFormatada.replace(/,/g, ' |');
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  return (
+   <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', flexDirection:'column' }}>
+  
+    <div>
+      <p className="horaP">{dataHoraFormatada}</p>
+
+      
+    </div>
+    <div className="divLink" style={{ }} >
+        <Link className="link" href="/">Voltar</Link>
+   
+      </div>
+   </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,37 @@
 import Link from "next/link"
 
-type Time = {
-  datetime: string
-}
+// type Time = {
+//   datetime: string
+// }
 
-async function getTime(): Promise<Time> {
-  const res = await fetch('http://www.worldtimeapi.org/api/timezone/America/Sao_Paulo', {next: {revalidate: 25}})
+// async function getTime(): Promise<Time> {
+//   const res = await fetch('http://www.worldtimeapi.org/api/timezone/America/Sao_Paulo', {next: {revalidate: 25}})
 
-  return  res.json()
+//   return  res.json()
   
-}
-export default async function Home() {
-  const [time] = await Promise.all([getTime()])
-  // Converte a string de data em um objeto Date
-  const dateTime = new Date(time.datetime);
- //Formata a data e hora usando as opções de localização do Brasil
-  let dataHoraFormatada = dateTime.toLocaleString('pt-BR');
-  dataHoraFormatada = dataHoraFormatada.replace(/,/g, ' |');
-  const refreshPage = () => {
-    window.location.reload();
-  };
+//}
+export default  function Home() {
+//   const [time] = await Promise.all([getTime()])
+//   // Converte a string de data em um objeto Date
+//   const dateTime = new Date(time.datetime);
+//  //Formata a data e hora usando as opções de localização do Brasil
+//   let dataHoraFormatada = dateTime.toLocaleString('pt-BR');
+//   dataHoraFormatada = dataHoraFormatada.replace(/,/g, ' |');
+//   const refreshPage = () => {
+//     window.location.reload();
+//   };
 
   return (
-   <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+   <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' , flexDirection:'column',}}>
   
-    <div>
-      <p className="horaP">{dataHoraFormatada}</p>
+    <div className="divLink">
+      <Link className="link" href="exemplo1">EXEMPLO COM VALIDATE</Link>
     </div>
+    
+    <div className="divLink">
+      <Link className="link" href="exemplo2">EXEMPLO SEM VALIDATE</Link>
+    </div>
+    
    </div>
   )
 }
